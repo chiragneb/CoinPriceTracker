f50e52dc6aeb13620b191c4035a0f3624c9d3cc7
neb03x <neb.3391@gmail.com>
2022-12-05 12:40:52 +0530

    initial commit

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,8 +4,8 @@ import "./Footer.css"
 const Footer = () => {
     return (
         <div class="footer">
-            <p>Made with ❤️ by <a href = "" className = "link">Chirag Neb 🙏🏻</a></p>
-            <p><a href=""><i className = "fa fa-github fa-2x link">https://github.com/chiragneb</i></a></p>
+            <p>Made with ❤️ by <a href = "https://github.com/chiragneb" className = "link">Chirag Neb </a></p>
+       
         </div>
     )
 }
