604f95b6924b6b8763eb85e1fe223df665b022e3
neb03x <neb.3391@gmail.com>
2022-12-05 12:34:39 +0530

    initial commit

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <div class="footer">
+            <p>Made with ❤️ by <a href = "" className = "link">Chirag Neb 🙏🏻</a></p>
+            <p><a href=""><i className = "fa fa-github fa-2x link">https://github.com/chiragneb</i></a></p>
+        </div>
+    )
+}
+
+export default Footer;
